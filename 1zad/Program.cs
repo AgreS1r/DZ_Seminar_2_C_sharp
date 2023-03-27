1dd4e7fdb17db4c03c474484ab600fdd2663e00c
danil <faceitplay2004@mail.ru>
2023-03-27 22:56:31 +0300

    Задания 10, 13, 15

---
+++ 1zad/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Enter number (100-999) ");
+int num = int.Parse(Console.ReadLine());
+
+int numA = num / 10;
+int result = numA % 10;
+Console.WriteLine(result);
---
+++ 3zad/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter number (1-7) ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num <= 7) {
+    if (num >= 6 && num <= 7){
+    Console.WriteLine("Да, выходной");
+}
+else{
+    Console.WriteLine("Нет, будний");
+}
+}
+else{  
+    Console.WriteLine("Enter number (1-7)!!!");
+}
+
+
+
